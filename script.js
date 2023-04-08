7cffb21b9166f73946fc614bb48f587a0837368b
sanketyelave <124805576+sanketyelave@users.noreply.github.com>
2023-04-09 05:26:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+'use strict';
+
+let show= document.querySelector('.check');
+let overlay= document.querySelector('.overlay');
+let hidden= document.querySelector('.hidden');
+let modal = document.querySelector('.modal');
+let close = document.querySelector('.close');
+let height=parseInt(document.querySelector('.height').value);
+let weight=parseInt(document.querySelector('.weight').value);
+let sanket=(weight/height**2);
+
+
+let ram=function(){ 
+
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+let sita=function(){
+    
+   modal.classList.add('hidden');
+   overlay.classList.add('hidden');
+}
+let esc=function(e){
+    if(e.key==='Escape'){
+        again();
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    }
+}
+
+let enter=function(e){
+   if(e.key==='Enter'){
+       modal.classList.remove('hidden');
+       overlay.classList.remove('hidden');
+   
+let height=parseInt(document.querySelector('.height').value);
+let weight=parseInt(document.querySelector('.weight').value);
+let sanket=(weight/height**2);
+   if(height!=height && weight!=weight ){
+    document.querySelector('.value').textContent = 'Enter a valid input☠️';
+}else{
+    document.querySelector('.value').textContent = `${sanket}✨`;
+}
+modal.classList.remove('hidden');
+overlay.classList.remove('hidden');
+}}
+
+let jay=function(){
+    let height=parseInt(document.querySelector('.height').value);
+let weight=parseInt(document.querySelector('.weight').value);
+let sanket=(weight/height**2);
+    if(!height && !weight && height===0){
+        document.querySelector('.value').textContent = 'Enter a valid input☠️';
+    }else{
+        document.querySelector('.value').textContent = `${sanket}✨`;
+    }
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+   ram();
+}
+let again=function(){
+    modal.classList.add('hidden');
+   overlay.classList.add('hidden');
+    let height=parseInt(document.querySelector('.height').value=0.0);
+    let weight=parseInt(document.querySelector('.weight').value=0.0);
+    let sanket=(weight/height**2);
+    document.querySelector('.weight').textContent = ' ';
+    document.querySelector('.height').textContent = ' ';
+    document.querySelector('.value').textContent = 'Enter a valid input☠️';
+    
+}
+
+  show.addEventListener('click',jay);
+   close.addEventListener('click',again);
+    overlay.addEventListener('click',again);
+    document.addEventListener('keydown',esc);
+    document.addEventListener('keydown',enter);
+
+
+
